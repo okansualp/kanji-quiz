be237f7f6867ff47f2eebe2b16e08224aeedd2f0
okansualp <okansualp1996@gmail.com>
2026-06-20 13:08:28 +0000

    Kelime-bazlı ustalık (mastery) ilerleme sistemi + PWA

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'kanji-quiz-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.put('./__marker', new Response('ok'));
+    await Promise.allSettled(ASSETS.map(a => c.add(a)));
+    await self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+         .then(() => self.clients.claim())
+  );
+});
+
+// Cache-first: çevrimdışı çalışır, çevrimiçiyse cache'i tazeler
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
